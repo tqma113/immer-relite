b86b8693fe65fc6cdb54afd9fe47dd3737a907cb
matianqi.tqma <matianqi.tqma@bytedance.com>
2022-01-13 21:08:42 +0800

    fix: attachDevTool by send

--- src/attachDevTool.ts
+++ src/attachDevTool.ts
@@ -1,4 +1,4 @@
-import type { Store, Data, Actions } from "./index";
+import type { Store, Actions } from "./index";
 
 declare global {
   interface Window {
@@ -12,44 +12,25 @@ export const attachDevTool = <S extends object, AS extends Actions<S>>(
   if (process.env.NODE_ENV === "production") {
     return;
   }
-  if (typeof window === "undefined" || !window.__REDUX_DEVTOOLS_EXTENSION__) {
+
+  if (typeof window === "undefined" || !window.__REDUX_DEVTOOLS_EXTENSION__ || !window.__REDUX_DEVTOOLS_EXTENSION__.send) {
     return;
   }
 
-  const __FROM_REDUX_DEVTOOLS_EXTENSION__ = "__FROM_REDUX_DEVTOOLS_EXTENSION__";
+  const sendMessage = window.__REDUX_DEVTOOLS_EXTENSION__.send
 
-  const options = {
+  const config = {
     name: window.location.pathname + window.location.search,
     actionsWhitelist: Object.keys(store.actions),
   };
-  const reduxStore = window.__REDUX_DEVTOOLS_EXTENSION__(
-    store.getState,
-    store.getState(),
-    options
-  );
-  let isSync = false;
-  store.subscribe((data?: Data<S, AS>) => {
-    if (!data || data.actionType === __FROM_REDUX_DEVTOOLS_EXTENSION__) {
-      return;
-    }
-    isSync = true;
-    reduxStore.dispatch({
-      type: data.actionType,
-      payload: data.actionPayload,
-    });
-    isSync = false;
-  });
 
-  reduxStore.subscribe(() => {
-    if (!isSync) {
-      store.replaceState(reduxStore.getState(), {
-        actionType: __FROM_REDUX_DEVTOOLS_EXTENSION__,
-        actionPayload: {},
-        previousState: store.getState(),
-        currentState: reduxStore.getState(),
-        start: new Date(),
-        end: new Date(),
-      } as Data<S, AS>);
-    }
+  store.subscribe((data) => {
+    sendMessage({
+      action: {
+        type: data.actionType,
+        payload: data.actionPayload
+      },
+      timestamp: data.end.getTime() - data.start.getTime(),
+    }, data.currentState, config)
   });
 };
